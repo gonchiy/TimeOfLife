1919bc812234cf71e3b16968f64dbaeb1b6801e2
gonchiy <daroga@ukr.net>
2020-03-26 22:12:11 +0200

    TimeOfLife counter

--- src/useTimeOfLife.java
+++ src/useTimeOfLife.java
@@ -2,15 +2,15 @@ public class useTimeOfLife {
 
     public static void main(String[] args){
 
-        //String infoString = "05 03 2015";
-        String infoString = "";
+        String infoString = "05 03 2015";
+        //String infoString = "";
 
         if (args.length > 0) {
             String stringFromComandLine = "";
             for (String d:args) stringFromComandLine = stringFromComandLine.concat(d + " ");
             new TimeOfLife(stringFromComandLine);
         } else {
-            if ((infoString != "") && (infoString != null)) new TimeOfLife(infoString);
+            if ((!infoString.equals("")) && (infoString != null)) new TimeOfLife(infoString);
                 else new TimeOfLife();
         }
 
